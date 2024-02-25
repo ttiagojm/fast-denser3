4bd712fc9ccc9e6699f1f9210ffcec44620da75c
ttiagojm <tiagodeha@protonmail.com>
2024-02-25 16:18:54 +0000

    Only ReLU activations

--- test.py
+++
@@ -1,5 +0,0 @@
-import fast_denser
-
-fast_denser.search(0, 'cifar10', 
-'D:\\Engenharia Informática\\3 Ano\\2 Semestre\Projeto II\\fast-denser3\\example\\config.json', 
-'D:\\Engenharia Informática\\3 Ano\\2 Semestre\\Projeto II\\fast-denser3\\example\\cnn.grammar')
